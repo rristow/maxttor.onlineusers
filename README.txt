cd89b8e6cdb51990a927c7ba230a910acb4258ac
unknown <rodrigo.ristow@WI21AM00086.adm.ds.fhnw.ch>
2014-12-30 22:39:53 +0100

    setup.cfg

--- README.txt
+++ README.txt
@@ -4,3 +4,6 @@ maxttor.onlineusers
 Plone product to restrict the number of sessions of an user. 
 The goal is to avoid that too many person use the same user-id. 
 (It was developed for one plone instance and won't work properly with Zeo)
+
+
+
---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.1rc6'
+
+setup(name='maxttor.onlineusers',
+      version=version,
+      description="Plone Product - Control the number of sessions for each user",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='Plone sessions control online',
+      author='Rodrigo Ristow',
+      author_email='rodrigo@maxttor.com',
+      url='https://github.com/rristow/maxttor.onlineusers',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['ityou'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
